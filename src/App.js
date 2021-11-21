4f324d0be829fff0afe4150fab0c347db1913144
RodrigoUngo <00075419@uca.edu.sv>
2021-11-20 19:16:49 -0600

    Add fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [activities, setActivities] = useState([]);
+
+  const fetchActivities = async () => {
+    const url = `${process.env.REACT_APP_API_URL}/activities`;
+
+    try {
+      const response = await fetch(url);
+      const obj = await response.json();
+      const results = obj.data;
+      setActivities(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchActivities();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
